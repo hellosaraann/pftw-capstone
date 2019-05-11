f1afd2b52f932431b4f5d381f2a1046864a17e1c
hellosaraann <srichardson@mcad.edu>
2019-05-10 23:03:27 -0500

    Added shuffle function & button

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
+// SHUFFLE FUNCTION
+
+// let shuffleCards = function(array) {
+//   array.sort(function() {return 0.5 - Math.random()})
+// };
+
 // CARD ARRAY
 
 const cardArray = [
-   {'index': 1 , 'text': 'red'},
-   {'index': 2 , 'text': 'orange'},
-   {'index': 3 , 'text': 'yellow'},
-   {'index': 4 , 'text': 'green'},
-   {'index': 5 , 'text': 'blue'},
-   {'index': 6 , 'text': 'purple'},
-   {'index': 7 , 'text': 'black'},
-   {'index': 8 , 'text': 'brown'},
-   {'index': 9 , 'text': 'red'},
-   {'index': 10 , 'text': 'orange'},
-   {'index': 11 , 'text': 'yellow'},
-   {'index': 12 , 'text': 'green'},
-   {'index': 13 , 'text': 'blue'},
-   {'index': 14 , 'text': 'purple'},
-   {'index': 15 , 'text': 'black'},
-   {'index': 16 , 'text': 'brown'}
+   {'index': 1 , 'color': 'red', 'text': '?'},
+   {'index': 2 , 'color': 'orange', 'text': '?'},
+   {'index': 3 , 'color': 'yellow', 'text': '?'},
+   {'index': 4 , 'color': 'green', 'text': '?'},
+   {'index': 5 , 'color': 'blue', 'text': '?'},
+   {'index': 6 , 'color': 'purple', 'text': '?'},
+   {'index': 7 , 'color': 'black', 'text': '?'},
+   {'index': 8 , 'color': 'brown', 'text': '?'},
+   {'index': 9 , 'color': 'red', 'text': '?'},
+   {'index': 10 , 'color': 'orange', 'text': '?'},
+   {'index': 11 , 'color': 'yellow', 'text': '?'},
+   {'index': 12 , 'color': 'green', 'text': '?'},
+   {'index': 13 , 'color': 'blue', 'text': '?'},
+   {'index': 14 , 'color': 'purple', 'text': '?'},
+   {'index': 15 , 'color': 'black', 'text': '?'},
+   {'index': 16 , 'color': 'brown', 'text': '?'}
 ];
 
 // CARD template
@@ -26,8 +32,8 @@ Vue.component ( 'single-card', {
 		'cards', 'text'
 	],
 	template: `
-		<div  class='card'>
-			<p>{{ text }}</p>
+		<div class='card'>
+			<h2>{{ text }}</h2>
 		</div>
 	`
 });
@@ -40,5 +46,10 @@ var vm = new Vue({
     cards: cardArray
   },
 	methods: {
+    shuffleCards: function(array) {
+      array.sort(function() {
+        return 0.5 - Math.random()
+      })
+    }
 		}
 });
