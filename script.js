66a7a79e4edca217c523bcd39248191a10333463
hellosaraann <srichardson@mcad.edu>
2019-05-11 20:26:29 -0500

    Compare cards functions working!

--- script.js
+++ script.js
@@ -1,28 +1,22 @@
-// SHUFFLE FUNCTION
-
-// let shuffleCards = function(array) {
-//   array.sort(function() {return 0.5 - Math.random()})
-// };
-
 // CARD ARRAY
 
 var cardArray = [
-   {'index': 1 , 'color': 'red', 'text': '?', 'selected': false},
-   {'index': 2 , 'color': 'orange', 'text': '?', 'selected': false},
-   {'index': 3 , 'color': 'yellow', 'text': '?', 'selected': false},
-   {'index': 4 , 'color': 'green', 'text': '?', 'selected': false},
-   {'index': 5 , 'color': 'blue', 'text': '?', 'selected': false},
-   {'index': 6 , 'color': 'purple', 'text': '?', 'selected': false},
-   {'index': 7 , 'color': 'black', 'text': '?', 'selected': false},
-   {'index': 8 , 'color': 'brown', 'text': '?', 'selected': false},
-   {'index': 9 , 'color': 'red', 'text': '?', 'selected': false},
-   {'index': 10 , 'color': 'orange', 'text': '?', 'selected': false},
-   {'index': 11 , 'color': 'yellow', 'text': '?', 'selected': false},
-   {'index': 12 , 'color': 'green', 'text': '?', 'selected': false},
-   {'index': 13 , 'color': 'blue', 'text': '?', 'selected': false},
-   {'index': 14 , 'color': 'purple', 'text': '?', 'selected': false},
-   {'index': 15 , 'color': 'black', 'text': '?', 'selected': false},
-   {'index': 16 , 'color': 'brown', 'text': '?', 'selected': false}
+   {'index': 1 , 'color': 'red', 'text': '?', 'selected': 'unselected'},
+   {'index': 2 , 'color': 'orange', 'text': '?', 'selected': 'unselected'},
+   {'index': 3 , 'color': 'yellow', 'text': '?', 'selected': 'unselected'},
+   {'index': 4 , 'color': 'green', 'text': '?', 'selected': 'unselected'},
+   {'index': 5 , 'color': 'blue', 'text': '?', 'selected': 'unselected'},
+   {'index': 6 , 'color': 'purple', 'text': '?', 'selected': 'unselected'},
+   {'index': 7 , 'color': 'black', 'text': '?', 'selected': 'unselected'},
+   {'index': 8 , 'color': 'brown', 'text': '?', 'selected': 'unselected'},
+   {'index': 9 , 'color': 'red', 'text': '?', 'selected': 'unselected'},
+   {'index': 10 , 'color': 'orange', 'text': '?', 'selected': 'unselected'},
+   {'index': 11 , 'color': 'yellow', 'text': '?', 'selected': 'unselected'},
+   {'index': 12 , 'color': 'green', 'text': '?', 'selected': 'unselected'},
+   {'index': 13 , 'color': 'blue', 'text': '?', 'selected': 'unselected'},
+   {'index': 14 , 'color': 'purple', 'text': '?', 'selected': 'unselected'},
+   {'index': 15 , 'color': 'black', 'text': '?', 'selected': 'unselected'},
+   {'index': 16 , 'color': 'brown', 'text': '?', 'selected': 'unselected'}
 ];
 
 // CARD template
@@ -44,12 +38,8 @@ var vm = new Vue({
   el: "#cardApp",
   data: {
     cards: cardArray,
-    selectedCards: []
-    // selectedCard: function(){
-    //   for( i = 0 ; i < this.cards.length ; i++ ){
-    //     return card[i].index;
-    //   }
-    // }
+    selectedCards: [],
+    matchedCards: []
   },
   methods: {
       shuffleCards: function(){
@@ -57,11 +47,32 @@ var vm = new Vue({
         return 0.5 - Math.random()
         })
       },
+      compareCards: function(){
 
+          if(this.selectedCards[0].color === this.selectedCards[1].color){
+              this.selectedCards[0].text = this.selectedCards[1].text = 'yay';
+              this.selectedCards[0].selected = this.selectedCards[1].selected= 'matched';
+              this.matchedCards.push(this.selectedCards[0], this.selectedCards[1]);
+              this.selectedCards.pop();
+              this.selectedCards.pop();
+          } else if(this.selectedCards[0].color !== this.selectedCards[1].color){
+              this.selectedCards[0].text = this.selectedCards[1].text = 'X';
+              setTimeout(function(){
+                    this.selectedCards[0].selected = this.selectedCards[1].selected = 'unselected';
+                    this.selectedCards[0].text = this.selectedCards[1].text = '?';
+                    this.selectedCards.pop();
+                    this.selectedCards.pop();
+              }.bind(this), 1000);
+          }
+      },
       displayCard: function(cardSelected){
-        cardSelected.selected = true;
+        cardSelected.selected = 'selected';
+        this.selectedCards.push(cardSelected);
+        if( this.selectedCards.length === 2 ){
+            this.compareCards();
+        }
       }
-		},
+  },
   created: function(){
     this.shuffleCards();
   }
